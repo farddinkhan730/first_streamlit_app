b902afd8acf7a0af1169648c1082f563771f377b
farddinkhan730 <60227471+farddinkhan730@users.noreply.github.com>
2023-11-16 16:39:21 +0530

    Update streamlit_app.py
    indent resolve

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,8 @@ streamlit.write('The user entered ', fruit_choice)
 #create the function
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice) 
-    fruityvice_normalized=pandas.json_normalize(fruityvice_response.json()) 
-    return fruityvice_normalized
+  fruityvice_normalized=pandas.json_normalize(fruityvice_response.json()) 
+  return fruityvice_normalized
 #new section
 streamlit.header('Fruityvice Fruit Advice!')
 try:
